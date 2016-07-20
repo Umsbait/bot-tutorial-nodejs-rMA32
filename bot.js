86a1a006a0fadb25e941945030393a6f8a9f9bbc
Umsbait <umsbaav@gmail.com>
2016-07-20 16:31:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
       botRegexAd=/^\/work/;botRegexGTA = /^\/classchecks/; botRegexSC = /^\/google/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/tickets/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/help/;
       botRegexSiege = /^\/siege/; botRegexB = /^\/schedule/; botJohn = /^\/john/; botFaculty = /^\/faculty/; botInventory = /^\/inventory/;
-      botSOS = /^\/sos/; 
+      botSOS = /^\/sos/; botScheduleForm = /^\/ScheduleForm/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -23,6 +23,11 @@ function respond() {
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://docs.google.com/document/d/1WoarNHEFTHRl410sORAdNsJPQXzm16JgrXEX7FXuLMk/edit?usp=sharing");
+    this.res.end();
+  } 
+  else if(request.text && botScheduleForm.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.tinyurl.com/fallhoursform");
     this.res.end();
   } 
   else if(request.text && botFaculty.test(request.text)) {
