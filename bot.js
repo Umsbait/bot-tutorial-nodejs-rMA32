705abf6b47bf9f2b6aaecee9fdb8f240c4a051d6
Umsbait <umsbaav@gmail.com>
2017-03-23 14:52:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function respond() {
       botSOS = /^\/sos/; botRegexP = /^\/cashme/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7';
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd'; google1 = 'http://bfy.tw/AoZK'; google2 = 'http://bfy.tw/3won';
-      google3 = 'http://bfy.tw/Aoak'; google4 = 'http://bfy.tw/AobV';
+      google3 = 'http://bfy.tw/AobV';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -42,8 +42,6 @@ function respond() {
       postMessage(google1);
     else if(Math.random() >0.6)
       postMessage(google3)
-    else if(Math.random() >0.3)
-      postMessage(google4)
     else
       postMessage(google2);
     this.res.end();
