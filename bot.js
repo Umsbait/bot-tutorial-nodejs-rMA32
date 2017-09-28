8b7ffbb28dd90d374cf7fde841aa1f54669ba845
Umsbait <umsbaav@gmail.com>
2017-09-28 13:01:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ function respond() {
       botRegexSb = /^\/tickets/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/help/;
       botRegexSiege = /^\/siege/; botRegexB = /^\/schedule/; botPdrive = /^\/pdrive/; botFaculty = /^\/faculty/; botInventory = /^\/inventory/;
       botSOS = /^\/sos/; botRegexP = /^\/cashme/;
+      botAVForm = /^\/avform/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7';
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd'; google1 = 'http://bfy.tw/AoZK'; google2 = 'http://bfy.tw/3won';
       google3 = 'http://bfy.tw/AobV'; 
@@ -39,6 +40,11 @@ function respond() {
     else if(request.text && botRegexPercocet.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://drive.google.com/file/d/0B5JhXcgmXyMZQU9idUpxQVhTRXc/view");
+    this.res.end();
+  }
+   else if(request.text && botAVForm.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.bus.miami.edu/avEvent");
     this.res.end();
   }
   else if(request.text && botRegexSalt.test(request.text)) {
