d84f5548f0b416e1902d83e3f6e46697ebf3da2d
Jorge Banegas <jjb87@miami.edu>
2017-10-30 16:27:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ function respond() {
       botRegexSb = /^\/tickets/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/help/;
       botRegexSiege = /^\/siege/; botRegexB = /^\/schedule/; botPdrive = /^\/pdrive/; botFaculty = /^\/faculty/; botInventory = /^\/inventory/;
       botSOS = /^\/sos/; botRegexP = /^\/cashme/; botRegexL = /^\/L/; botRegexpeach = /^\/peaches/;
-      botAVForm = /^\/avform/;
+      botAVForm = /^\/avform/; botBibu = /^\/bibush/;
       botBBC=/^\/bbc/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7';
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd'; google1 = 'http://bfy.tw/AoZK'; google2 = 'http://bfy.tw/3won';
@@ -195,6 +195,12 @@ function respond() {
       postMessage(siege2);
     this.res.end();
   }
+ else if(request.text && botBibu.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://i.groupme.com/500x260.gif.bacbe4d6c75344ad8aacb4e8bc169349.large");
+    this.res.end();
+  }
+  
   
   else {
     console.log("don't care");
