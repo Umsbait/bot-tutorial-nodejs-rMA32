500540eb6a21d06613afa9c742470d05b5aadc82
Umsbait <umsbaav@gmail.com>
2017-12-31 16:21:19 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ function respond() {
   }
   else if(request.text && botHelp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("When typing a command, use a '/' instead of a dash"/n);
+    postMessage("When typing a command, use a '/' instead of a dash");
    // postMessage("The following [useful] commands are as follows:"/n);
    // postMessage("-google, -script, -schedule, -spring, -users, -pdrive, -faculty, -inventory, -internetconfig, -directory, -avform, -HDForm"/n);
     this.res.end();
