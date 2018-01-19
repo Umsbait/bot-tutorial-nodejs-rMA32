92c41c24c2ed66e66e29d470c9cea3cb10ff3806
Umsbait <umsbaav@gmail.com>
2018-01-19 12:12:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,7 @@ function respond() {
       botSOS = /^\/sos/; botRegexP = /^\/cashme/; botRegexL = /^\/L/; botRegexPooja = /^\/pooja/;
       botAVForm = /^\/avform/; botBibu = /^\/bibush/; botHDForm = /^\/HDForm/;botSpringS = /^\/Spring/;botHelp= /^\/help/;
       botWinter=/^\/Winter/;
+      botFormD=/^\/checklist/;
       botCentral=/^\/Central/;botcentral2=/^\/central/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7';
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd'; google1 = 'http://bfy.tw/AoZK'; google2 = 'http://bfy.tw/3won';
@@ -27,6 +28,11 @@ function respond() {
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://drive.google.com/open?id=1tES5uRM7D9x2KLcG-qf8mVhFxHSMls_AggoswbnLLgA");
+    this.res.end();
+  }
+  else if (request.text && botFormD.test(request.text)) {
+   this.res.writeHead(200);
+    postMessage("https://goo.gl/forms/thtOAvPkhB18kVZJ3");
     this.res.end();
   }
   else if (request.text && botCentral.test(request.text)) {
